a15d1d010bee4b63a49fcfa9dc97c1979e300aef
souhailaarab98 <souhailaarab98@gmail.com>
2021-10-27 16:23:22 +0200

    initial commit

---
+++ ejercicio8.7.1.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+listaDeso = [7, 8, 9, 4, 15, 14, 2, 0, 4, 4, 3]
+elemento = 4
+
+
+def cantidadCoincidencias(lista, elemento):
+    contador = 0
+    for i in lista:
+        if i == elemento:
+            contador += 1
+    return contador
+
+def posicionDePrimera(lista, elemento):
+    posicion = 0
+    encontrado = False
+    for i in lista:
+        if i == elemento and encontrado == False:
+            return posicion
+            encontrado = True
+        posicion += 1
+
+def listaPosiciones(lista, elemento):
+    lista2 = []
+    posicion = 0
+    for i in lista:
+        if i == elemento:
+            lista2.append(posicion)
+        posicion += 1
+    return lista2
+
+print cantidadCoincidencias(listaDeso, elemento)
+print posicionDePrimera(listaDeso, elemento)
+print listaPosiciones(listaDeso, elemento)
